8d652a916687946c747508e42f8668988b2c2ba4
Michelle Olivieri <michelleo72@gmail.com>
2018-03-15 14:52:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,11 +17,11 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
+    if !position_taken(board, index).all? do
     if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
       return win_combo
     elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
       return win_combo
-
     end
   end
 end
