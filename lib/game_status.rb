3e8803e4f766e78752a1735e060323c765a9c836
Michelle Olivieri <michelleo72@gmail.com>
2018-03-15 15:36:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ def won?(board)
     # position_taken?(board, index)
     if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
       return win_combo
-    if board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
+    elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
       return win_combo
     else
       return false
