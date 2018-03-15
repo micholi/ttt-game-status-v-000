b02f9d7de704333d12f54780735cd168e645ef19
Michelle Olivieri <michelleo72@gmail.com>
2018-03-15 15:33:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,7 +23,6 @@ def won?(board)
     elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
       return win_combo
     else
-      false
     end
   end
 end
