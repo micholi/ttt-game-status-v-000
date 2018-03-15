5f1b649ad6bc043cc1ad4d6b3256823a707fd660
Michelle Olivieri <michelleo72@gmail.com>
2018-03-15 12:44:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,6 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
   [6,7,8], # Bottom row
-  # ETC Remaining combinations below
   # Left column
   # Middle column
   # Right colum
