4627600df2eff727ffe3f15ad8d41bd9934fa9d0
Michelle Olivieri <michelleo72@gmail.com>
2018-03-15 14:30:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,6 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
-    # win_index_1 = win_combo[0]
-    # win_index_2 = win_combo[1]
-    # win_index_3 = win_combo[2]
     position_1 = board[win_combo[0]]
     position_2 = board[win_combo[1]]
     position_3 = board[win_combo[2]]
